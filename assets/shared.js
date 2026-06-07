451d4977374759be7d3b556c5d5bfe64f67efb06
Motolani Akala <mendoakala@yahoo.com>
2026-06-07 19:55:39 +0100

    commit

--- assets/shared.js
+++ assets/shared.js
@@ -9,3 +9,58 @@ fetch("../components/navbar.html")
     ).innerHTML = data;
 
 });
+
+const themeToggle =
+    document.getElementById(
+        "themeToggle"
+    );
+
+if (
+    localStorage.getItem(
+        "theme"
+    ) === "light"
+) {
+
+    document.body.classList.add(
+        "light"
+    );
+
+}
+
+document.addEventListener(
+    "click",
+    event => {
+
+        if (
+            event.target.id ===
+            "themeToggle"
+        ) {
+
+            document.body.classList.toggle(
+                "light"
+            );
+
+            if (
+                document.body.classList.contains(
+                    "light"
+                )
+            ) {
+
+                localStorage.setItem(
+                    "theme",
+                    "light"
+                );
+
+            } else {
+
+                localStorage.setItem(
+                    "theme",
+                    "dark"
+                );
+
+            }
+
+        }
+
+    }
+);
